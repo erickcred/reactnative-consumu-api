664ac183f2ef9f1f60996f837fba9f7aad93315a
Erick Rick <erickcredjes@gmail.com>
2025-08-16 09:58:31 -0300

    implementando carDetails

--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -3,16 +3,31 @@ import { Text, View, Button, Image } from "react-native";
 
 import { styles } from "./styles";
 import logo from "../../../assets/logo.png";
+import Divider from "../Divider";
 
 export default function CardView() {
+
+  const renderLogoBox = () => {
+    return (
+      <View style={styles.logoContainer}>
+        <Image style={styles.imageLogo} source={logo} />
+      </View>
+    );
+  }
+
+  const renderCarDetails = () => (
+    <View style={{ alignItems: "center" }}>
+      <Text style={styles.carBrand}>Lamborghini</Text>
+      <Text style={styles.carName}>MODEL</Text>
+    </View>
+  )
+
   return (
     <View style={styles.imageContainer}>
-      <View style={styles.logoContainer}>
-        <Image
-          style={styles.imageLogo}
-          source={logo}
-        />
-      </View>
+      {renderLogoBox()}
+      
+      <Divider />
+      {renderCarDetails()}
     </View>
   )
 }
